c4e51b4e2db8bff0967279aeecccfd8a89b283fe
chazool <chazoolkaweesha@gmail.com>
2023-03-04 12:14:37 +0530

    update swagger

--- common/cmd/commonservice/commonservice.go
+++ common/cmd/commonservice/commonservice.go
@@ -1,10 +1,24 @@
 package main
 
-import "github.com/chazool/go-sample-app/common/pkg/config"
+import (
+	_ "github.com/chazool/go-sample-app/common/docs"
+	"github.com/chazool/go-sample-app/common/pkg/config"
+)
 
 func init() {
 	config.InitConfig()
 }
+
+// @title Sample Service
+// @version 1.0
+// description Restful service for sample
+
+// @contact.name Chazool
+// @contact.url  https://lk.linkedin.com/in/chazool
+// @contact.email chazoolk@gmail.com
+
+//BasePath /api/v1
+//@schemes https
 
 func main() {
 	config.Start()
